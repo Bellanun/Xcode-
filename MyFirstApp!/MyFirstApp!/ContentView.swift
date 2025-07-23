8102cf24d9470977e0774345bfbabe778b95d6fa
Bellanun <isanun0812@gmail.com>
2025-07-23 11:16:29 -0400

    Add files via upload

---
+++ MyFirstApp!/MyFirstApp!/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  MyFirstApp!
+//
+//  Created by Scholar on 7/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("this is my Tucan")
+      Image("Tucan")
+        Text("His name is Bobby")
+        
+        
+        
+        
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyFirstApp!/MyFirstApp!/MyFirstApp_App.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp_App.swift
+//  MyFirstApp!
+//
+//  Created by Scholar on 7/22/25.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
