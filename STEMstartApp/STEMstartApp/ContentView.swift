ef50650f5d3a56cb451470ae57fa3341b102193f
Isabella Nunez <isanun0812@gmail.com>
2025-07-31 15:37:02 -0400

    Add files via upload

---
+++ STEMstartApp/STEMstartApp/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  STEMstartApp
+//
+//  Created by Scholar on 7/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "Marie Curie")
+            Color(red: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/, green: 0.6, blue: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/)
+        }
+        Text("Marie Curie")
+        
+       Image("Marie Curie")
+            
+        
+        Color(red: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/, green: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/, blue: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/)
+        
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
---
+++ STEMstartApp/STEMstartApp/STEMstartAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  STEMstartAppApp.swift
+//  STEMstartApp
+//
+//  Created by Scholar on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct STEMstartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+         Image("marie curie")
+        }
+    }
+}
