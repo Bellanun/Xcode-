6ae9658e27c7489913e48a186008632ba9c9752b
Isabella Nunez <isanun0812@gmail.com>
2025-07-31 15:35:09 -0400

    Add files via upload

---
+++ InteractiveUIs/InteractiveUIs/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  InteractiveUIs
+//
+//  Created by Scholar on 7/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var name = ""
+    @State private var textTitle = "I'd love to know your name!"
+    
+    
+    var body: some View {
+        NavigationStack {
+            
+        }
+        VStack {
+            Text(textTitle)
+          Text("this is my interactive UI")
+            TextField("type your name here scholar!", text: $name)
+            Button("click me") {
+                textTitle =
+                "welcome,\(name)"
+                name = ""
+                
+            
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
