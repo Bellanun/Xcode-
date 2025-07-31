3cbcee5de18588225d04af22990437fe06532212
Isabella Nunez <isanun0812@gmail.com>
2025-07-31 15:30:42 -0400

    Add files via upload

---
+++ 1_SwiftDataTypes.playground/Contents.swift
@@ -0,0 +1,27 @@
+print("hello,world")
+print("Im 16 years old!😊")
+print("Isabella Nunez")
+var favCereal="frosty flakes"
+print(favCereal)
+var favoriteColor="Navy Blue"
+print(favoriteColor)
+var pet="Blu"
+print(pet)
+var friend="Myles"
+print(friend)
+var goal="learn html"
+print(goal)
+var hobby="gardening"
+print(hobby)
+var listName = "reminders"
+listName = "monday to-do"
+print(listName)
+var color : String
+color = "pink"
+var lastNumber : Int
+lastNumber = 78
+
+var first = "Karlie"
+var last = "Kloss"
+print("\(first)\(last)")
+
