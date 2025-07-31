0ac30669ae10bf8787bb2f64b9fab611a8e3cc1c
Isabella Nunez <isanun0812@gmail.com>
2025-07-31 15:30:59 -0400

    Add files via upload

---
+++ integers and operators.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+var januaryAvg = 12
+var marchAvg = 65
+var novemberAvg = 31
+var julyAvg = 98
+
+//writing code find the average of these four numbers
+var average = (januaryAvg + marchAvg + novemberAvg + julyAvg) / 4
+print(average)
+
+let hourlyRate = 12.50
+let hoursWorked = 15.0
+
+
+let jadesPaycheck = hourlyRate * Double(hoursWorked)
+
+print("Jade worked \(hoursWorked) and earned \(jadesPaycheck)")
+
+
