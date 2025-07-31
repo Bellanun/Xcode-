0771c23992fd9ae0eee047bb5700b4abd4d8c233
Isabella Nunez <isanun0812@gmail.com>
2025-07-31 15:31:13 -0400

    Add files via upload

---
+++ 2_conditionalStatments.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+//decalre the variables firstName and age
+
+var firstName = "isabella"
+var age = 16
+
+//is your name the same name as karlie
+print(firstName == "karlie")
+
+//is this your age?
+print(age == 100)
+
+//is your age greater than 7?
+print(age > 7)
+
+var score = "90"
+
+if score == "90" {
+    print("you passed!😆")
+} else {
+    print("try again nest time 😉")
+}
+
