c6a3812501f65b21454defc05df4e68215fb66ba
Isabella Nunez <isanun0812@gmail.com>
2025-07-31 15:31:46 -0400

    Add files via upload

---
+++ 3_Arrays and Loops.playground/Contents.swift
@@ -0,0 +1,40 @@
+var scholar1 = "cecilia"
+var scholar2 = "linh"
+var scholar3 = "isabella"
+
+print(scholar1)
+
+//this is an array of my favorite foods
+var favoriteFood = ["spaghetti", "sushi", "pasta", "icecream", "salad"]
+
+
+//how to print the fist element in the array
+print(favoriteFood[0])
+//print the third element in the array
+print(favoriteFood[2])
+// print the second element in the array
+print(favoriteFood[1])
+
+favoriteFood.append("seoul city")
+print(favoriteFood)
+
+favoriteFood.remove(at: 1)
+print(favoriteFood)
+
+favoriteFood.append("options")
+print(favoriteFood)
+favoriteFood.append("APT")
+print(favoriteFood)
+favoriteFood.remove(at: 3)
+print(favoriteFood)
+
+
+
+
+//decalre an array of 5 friends
+var myFriends = ["Addy", "Nina", "Nat", "Fatima", "Olivia"]
+//write a for-in loop that prints a full sentence for each friend to the console
+for friend in myFriends {
+    print("One o f my friends is\()")
+}
+
