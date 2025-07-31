39fc639eff1dac05bc10cffbec3eefe7abdc2dd3
Isabella Nunez <isanun0812@gmail.com>
2025-07-31 15:36:01 -0400

    Add files via upload

---
+++ Gitpractice/Gitpractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Gitpractice
+//
+//  Created by Scholar on 7/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("change1")
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
